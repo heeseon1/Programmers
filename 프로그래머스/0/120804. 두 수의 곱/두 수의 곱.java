6961b5593fad5e9d2788dabf4760e0604173b847
heeseon <116551167+heeseon1@users.noreply.github.com>
2024-10-27 20:32:56 +0900

    [level 0] Title: 두 수의 곱, Time: 0.04 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱/두 수의 곱.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        if ((num1 >= 0 && num1 <= 100) && (num2 >=0 && num2 <= 100)){
+            answer = num1 * num2;
+        }else{
+            System.out.print("숫자는 0~100까지 입력해 주세요.");
+        }
+        
+        return answer;
+    }
+}
