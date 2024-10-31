450c6386959cf15735449ed0fc286ccd78220ea4
heeseon <116551167+heeseon1@users.noreply.github.com>
2024-10-31 14:52:11 +0900

    [Bronze III] Title: 곱셈, Time: 172 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+	
+		String bStr = String.valueOf(b);
+		
+		String slice3 = bStr.substring(2,3);
+		String slice2 = bStr.substring(1,2);
+		String slice1 = bStr.substring(0,1);
+		
+		int b3 = Integer.parseInt(slice3);
+		int b2 = Integer.parseInt(slice2);
+		int b1 = Integer.parseInt(slice1);
+		
+		System.out.println(a * b3);
+		System.out.println(a * b2);
+		System.out.println(a * b1);
+		System.out.println(a * b);
+		
+	}
+
+}
