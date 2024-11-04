793aedae1582adbd0b4b3137b0938f87dceb2bd0
heeseon <116551167+heeseon1@users.noreply.github.com>
2024-11-04 14:55:53 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int score = sc.nextInt();
-		
-		if(score >= 90) System.out.println("A");
-		else if(score >= 80 && score < 90) System.out.println("B");
-		else if(score >=70 && score < 80)System.out.println("C");
-		else if(score >=60 && score < 70)System.out.println("D");
-		else System.out.println("F");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int score = Integer.parseInt(br.readLine());
+
+        if(score >= 90) System.out.println("A");
+        else if(score >= 80) System.out.println("B");
+        else if(score >= 70) System.out.println("C");
+        else if(score >= 60) System.out.println("D");
+        else System.out.println("F");
+
+    }
 }
