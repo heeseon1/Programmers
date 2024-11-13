f07603430fbfdd9a3c6062fdeedfc906048a66ca
heeseon <116551167+heeseon1@users.noreply.github.com>
2024-11-13 17:27:25 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 16052 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args)    throws IOException {
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    	int t = Integer.parseInt(br.readLine());
+    	 int arr[] = new int[t];
+    	
+    	 for ( int i =0; i  <  t;  i ++) {
+    		 String[] inputs = br.readLine().split(" ");
+             int a = Integer.parseInt(inputs[0]);
+             int b = Integer.parseInt(inputs[1]);
+	    	arr[i] =   a + b;
+	    	}
+	       
+    	 	for(int j = 0; j < t; j++) {
+    	 		bw.write("Case #" + ( j+1) + ": " + arr[j] + "\n" );
+    	 	}
+
+	    	 bw.flush();
+	         bw.close();
+}
+    }
