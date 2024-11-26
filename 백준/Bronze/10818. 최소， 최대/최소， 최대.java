a8a8fcc36951bcd64abb11112f908bc4018f1f6c
heeseon <116551167+heeseon1@users.noreply.github.com>
2024-11-26 10:04:37 +0900

    [Bronze III] Title: 최소, 최대, Time: 464 ms, Memory: 106288 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,24 @@
-import java.io.BufferedReader; 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int n = Integer.parseInt(br.readLine());
 		String[] inputs =br.readLine().split(" ");
 		
-		int max =  Integer. parseInt(inputs[0]);
-		int min =  Integer. parseInt(inputs[0]);
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 		
 		for (int i = 0;  i  <  n;  i++) {
 			int value = Integer. parseInt(inputs[i]);
 			
 			if( value >  max) {
 				max = value;
-			} else if ( value < min ) {
+			}
+			if ( value < min ) {
 				min = value;
 			}
 		}
