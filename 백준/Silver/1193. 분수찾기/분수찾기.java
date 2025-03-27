a2f28a914131957cda0ec46b97c75b6380e8a5be
heeseon <116551167+heeseon1@users.noreply.github.com>
2025-03-27 15:03:17 +0900

    [Silver V] Title: 분수찾기, Time: 124 ms, Memory: 15984 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException  {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	int line = 1;
+    	int line = 0;
     	int sum = 0, num = 0;
     	int x = 0;
     	int y = 0;
@@ -14,19 +14,19 @@ public class Main {
     	int n = Integer.parseInt(br.readLine());
     	
     	// 계산
-    	for (int i = 1;  sum < n; i++) {
-    		sum += i;                        // 누적 합
-    		line = i ;                          // 대각선 줄
-    		num = n - (sum - i);     // 대각선 줄에서 몇 번째인지
+    	while( sum < n) {
+    		line ++;              // 대각선 줄
+    		sum = sum + line;     // 누적 합
     	}
+		num = n - (sum - line);   // 대각선 줄에서 몇 번째인지
     	
     	// 홀짝 구별
     	if( line % 2 == 0) {
-    		 x = 0 + num;
+    		 x =  num;
     		 y = line - num + 1;
     	}else{
     		 x = line - num + 1;
-    		 y = 0 + num;
+    		 y =  num;
     	}
 			
     	// 출력
